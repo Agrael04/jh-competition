71d77ea4ffe69f64458f25cf52c1d6bdf1c01b1a
dioxs <diomonix@yandex.ua>
2019-12-03 22:37:17 +0200

    add sending phone code verification

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,6 +11,26 @@ const app = firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE))
 
 const auth = app.auth()
 
+firebase.auth().useDeviceLanguage()
+
+const recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+  'size': 'invisible',
+  'callback': () => {
+    console.log()
+  },
+})
+
+let code = { confirm: console.log }
+
+auth.signInWithPhoneNumber('+380502745793', recaptchaVerifier)
+  .then(confirmationResult => {
+    code = confirmationResult
+  })
+  .then(() => {
+    return code.confirm('sad')
+  })
+  .catch(console.log)
+
 if (auth !== null) {
   console.log(auth.currentUser)
 }
