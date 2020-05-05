fe6b3794d26da7194a004eb9bbe491dddb302794
Agrael04 <dioxs.7z@gmail.com>
2020-05-05 08:52:11 +0300

    update login methods

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider } from '@apollo/react-hooks'
 import { Provider } from 'react-redux'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import ThemeProvider from '@material-ui/styles/ThemeProvider'
 import MomentUtils from '@date-io/moment'
 import 'moment/locale/uk'
 
-import client from 'api/mongodb/graphql'
-
-import { DndProvider } from 'react-dnd'
-import Backend from 'react-dnd-html5-backend'
-
 import store from './store'
+import theme from './theme'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
 
 import Routes from './routes'
 
-(window as any).apollo = client
-
-if (client) {
-  ReactDOM.render((
-    <DndProvider backend={Backend}>
-      <MuiPickersUtilsProvider utils={MomentUtils} locale='uk'>
-        <ApolloProvider client={client}>
-          <Provider store={store}>
-            <BrowserRouter>
-              <Routes />
-            </BrowserRouter>
-          </Provider>
-        </ApolloProvider>
-      </MuiPickersUtilsProvider>
-    </DndProvider>
-  ), document.getElementById('root'))
-}
+ReactDOM.render((
+  <MuiPickersUtilsProvider utils={MomentUtils} locale='uk'>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
+  </MuiPickersUtilsProvider>
+), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,61 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import ThemeProvider from '@material-ui/styles/ThemeProvider'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+import { HttpLink, InMemoryCache, ApolloClient } from 'apollo-boost'
+import { setContext } from 'apollo-link-context'
+
+import {
+  hasLoggedInUser,
+  loginAnonymous,
+  getCurrentUser,
+} from 'api/mongodb'
 
 import Schedule from './schedule'
 
 import Layout from '../components/layout'
-import theme from '../theme'
+
+const createClient = () => {
+  if (!hasLoggedInUser()) {
+    return null
+  }
+
+  const accessToken = getCurrentUser().auth.activeUserAuthInfo.accessToken
+
+  // Add an Authorization header to each GraphQL request
+  const authLink = setContext((_: any, { headers }: any) => ({
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${accessToken}`,
+    },
+  }))
+
+  // Connect Apollo to the GraphQL Endpoint
+  const GRAPHQL_URL = `https://stitch.mongodb.com/api/client/v2.0/app/mongodb-provider-gzjek/graphql`
+  const httpLink = new HttpLink({ uri: GRAPHQL_URL })
+
+  // Instantiate the Apollo Client
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  })
+
+  return client
+}
 
 export default function Routes() {
+  const [loggedIn, setLoggedIn] = React.useState(hasLoggedInUser())
+
+  if (!loggedIn) {
+    loginAnonymous().then(() => setLoggedIn(hasLoggedInUser))
+
+    return null
+  }
+
+  const client = createClient()!
+
   return (
-    <ThemeProvider theme={theme}>
+    <ApolloProvider client={client}>
       <Layout>
         <Switch>
           <Route path={'/schedule'} exact={true}>
@@ -20,6 +66,6 @@ export default function Routes() {
           </Route>
         </Switch>
       </Layout>
-    </ThemeProvider>
+    </ApolloProvider>
   )
 }
